a9eae4181732f59bf66002f6ca63a188cd7a1252
Shruti <shruti706_shruti@netzoptimize.com>
2025-11-04 18:06:39 +0530

    Updated project with new design changes

--- Ben Friedman/script.js
+++ Ben Friedman/script.js
@@ -15,3 +15,31 @@ $(document).ready(function(){
     }
   });
 });
+// Custom navigation buttons
+  $('.custom-next').click(function() {
+    owl.trigger('next.owl.carousel');
+  });
+  $('.custom-prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+  });
+
+$(document).ready(function(){
+  const $arrow = $(".arrow-indicator");
+
+  $(".logo-box").click(function(){
+    $(".logo-box").removeClass("active");
+    $(this).addClass("active");
+
+    // Move arrow below clicked logo
+    const logoOffset = $(this).position().left + ($(this).outerWidth() / 2) - ($arrow.width() / 2);
+    $arrow.css("left", logoOffset + "px");
+  });
+
+  // Position arrow initially under first logo
+  const $firstLogo = $(".logo-box").first();
+  const firstOffset = $firstLogo.position().left + ($firstLogo.outerWidth() / 2) - ($arrow.width() / 2);
+  $arrow.css("left", firstOffset + "px");
+});
+
+
+
